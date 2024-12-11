3f69df0d4f70b8599ee6f521ab8d70e6530cbf81
rami44 <53144875+rami44@users.noreply.github.com>
2024-12-11 11:18:16 +0000

    Update smash.cpp

--- smash.cpp
+++ smash.cpp
@@ -6,17 +6,22 @@
 #include "signals.h"
 
 int main(int argc, char *argv[]) {
+    
+ 
     if (signal(SIGINT, ctrlCHandler) == SIG_ERR) {
         perror("smash error: failed to set ctrl-C handler");
     }
 
-
     SmallShell &smash = SmallShell::getInstance();
+    
     while (true) {
-        std::cout << "smash> ";
+        std::cout << smash.getPrompt();
         std::string cmd_line;
         std::getline(std::cin, cmd_line);
+        
+        
         smash.executeCommand(cmd_line.c_str());
+        
     }
     return 0;
 }
